9c6ffe11d507f63f1c8c0cde1908a5428b661f8f
Christophe Laferriere <christophe.laferriere@gmail.com>
2010-12-28 17:07:00 +0100

    Now uses random generation, still need to improve

--- random_tree/tree.c
+++ random_tree/tree.c
@@ -2,6 +2,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int create_tree (igraph_t* graph, int desired_number_of_non_leaf_nodes, int min_children, int max_children)
+{
+  igraph_vector_t edgelist;
+  int next_parent = 0, next_child = 1, i;
+
+  igraph_vector_init(&edgelist, 0);
+
+  while (next_parent < desired_number_of_non_leaf_nodes) {
+    int num_children = rand() % (max_children - min_children + 1) + min_children;
+
+    if (next_parent >= next_child) {
+      /* You may simply break out of the loop here instead of printing an error */
+      IGRAPH_ERROR("ran out of potential parent nodes", IGRAPH_EINVAL);
+    }
+
+    for (i = 0; i < num_children; i++) {
+      IGRAPH_CHECK(igraph_vector_push_back(&edgelist, next_parent));
+      IGRAPH_CHECK(igraph_vector_push_back(&edgelist, next_child));
+      next_child++;
+    }
+
+    next_parent++;
+  }
+
+  igraph_create(graph, &edgelist, 0, 0);
+
+  return IGRAPH_SUCCESS;
+}
+
+
+
+
 int main(int argc, char** argv)
 {
   int ret;
@@ -23,9 +55,19 @@ int main(int argc, char** argv)
   else
     printf ("Tree created!\n");
 
-  igraph_write_graph_dot(&graph, fp);
+  //igraph_write_graph_dot(&graph, fp);
 
   igraph_destroy(&graph);
-  
+
+  // Usage :
+  igraph_t graph2;
+
+  srand(time(0));
+  create_tree(&graph2, 10, 0, 4);
+
+  igraph_write_graph_dot(&graph2, fp);
+
+  igraph_destroy(&graph2);
+
   return 0;
 }
