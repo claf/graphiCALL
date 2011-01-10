26f331972d9e3d360dd4484853db2f8f4336556b
Christophe Laferriere <christophe.laferriere@gmail.com>
2011-01-10 19:46:33 +0100

    now uses file from trees.pl script (which are not .dot file) to generate calls. still need to implement the "work" function.

--- cecilia_version/src/graphiCALL/joe.c
+++ cecilia_version/src/graphiCALL/joe.c
@@ -2,49 +2,103 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-
-#define my_rand() (int)( (DATA.max_childs + 1.0) * rand() / ( RAND_MAX + 1.0 ) )
+#include <timing.h>
 
 DECLARE_DATA {
-  int max_childs;
 };
 
 #include <cecilia.h>
 
-void METHOD(self, self_work)(void* _this, int childs, int layer)
+unsigned int** children;
+unsigned int* weights;
+unsigned int* sons;
+
+
+void work(int aow)
 {
-  int i;
+
+}
+
+void METHOD(self, self_work)(void* _this, int node)
+{
+  int i, round;
   static int dummy = 0;
+  //tick_t t1, t2;
 
-  if (layer != 0)
+  //GET_TICK (t1);
+
+
+  /* TODO : do your amount of work : */
+  work (weights[node]);
+
+
+  /* Then call your sons : */
+  for (i = 0; i < sons[node]; i++)
   {
-    __sync_fetch_and_add( &dummy, 1);
-    printf ("Layer %d\tRound %d\tChilds %d\n", layer, dummy, childs);
-
-    for (i = 0; i < childs; i++)
-    {
-      CALLMINE(self, self_work, my_rand (), layer-1);
-    }
+    printf ("CALLING %d, son of %d\n", children[node][i], node);
+    CALLMINE(self, self_work, children[node][i]);
   }
+
+  //GET_TICK (t2);
+  //printf ("\tTick diff for Round %d(%d) : %.0llu\n", round, dummy ,TICK_DIFF (t1, t2));
 }
 
 int METHOD(entry, main)(void *_this, int argc, char** argv)
 {
   unsigned int iseed = (unsigned int)time(NULL);
+  int i;
+
+  timing_init();
 
   srand (iseed);
 
-  if (argc != 3)
+  if (argc != 2)
   {
-    printf ("\tUsage : ./graphiCALL layer max_childs\n");
-    printf ("\t\tlayer : number of layers in generated tree\n");
-    printf ("\t\tmax_childs : number of max childs in generated tree\n");
+    printf ("\tUsage : ./graphiCALL filename\n");
+    printf ("\t\tfilename : filename of the generated .dot file\n");
     return 1;
   }
 
-  DATA.max_childs = atoi (argv[2]);
+  /* DOT file parsing : */
+  const char* filename = argv[1];
+  FILE* file = fopen (filename, "r");
 
-  CALLMINE(self, self_work, my_rand (), atoi(argv[1]));
+  //read total node number
+  unsigned int nodes_number;
+  fscanf(file, "%d", &nodes_number);
+  children = (unsigned int**) malloc(sizeof(unsigned int*) * nodes_number);
+  weights = (unsigned int*) malloc(sizeof(unsigned int*) * nodes_number);
+  sons = (unsigned int*) malloc(sizeof(unsigned int*) * nodes_number); 
+
+  unsigned int current_node;
+  unsigned int weight;
+  unsigned int number_of_children;
+
+
+  while (fscanf(file, "%d : %d / %d", &current_node, &weight, &number_of_children) != EOF)
+  {
+    weights[current_node] = weight;
+    printf ("Work for %d : %d\n", current_node, weights[current_node]);
+    sons[current_node] = number_of_children;
+    printf ("Sons for %d : %d\n", current_node, sons[current_node]);
+
+    if (number_of_children != 0)
+    {
+      children[current_node] = (unsigned int*) malloc(sizeof(unsigned int) * number_of_children);
+      for (i=0; i<number_of_children;i++)
+      {
+        int next;
+        fscanf(file, "%d,", &next);
+        children[current_node][i] = next;
+        printf ("Children [%d][%d] : %d\n", current_node, i, children[current_node][i]);
+      }
+    } else { // current_node is a leaf.
+      children[current_node] = NULL;
+    }
+  }
+
+  /* First CALL : */
+  CALLMINE(self, self_work, 0);
 
   return 0;
 }
